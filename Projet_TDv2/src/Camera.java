3a08580266e800d54e8306ad29a0bc317b443fb6
nabi955 <nabihoudine.amady@gmail.com>
2023-11-22 08:46:52 +0100

    version 1

---
+++ Projet_TDv2/src/Camera.java
@@ -0,0 +1,27 @@
+public class Camera {
+    private  int x;
+    private int y;
+
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Camera(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Camera{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
+
---
+++ Projet_TDv2/src/GameScene.java
@@ -0,0 +1,13 @@
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+public class GameScene extends Scene {
+
+    public GameScene(Parent parent, double v, double v1) {
+        super(parent, v, v1);
+    }
+
+
+
+}
